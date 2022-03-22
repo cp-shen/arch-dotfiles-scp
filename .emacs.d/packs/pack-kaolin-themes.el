a9286d2c85f442c998c7fe5210708e65f328197c
scp <scp@archlinuxscp.xyz>
2022-03-22 09:13:37 +0800

    remove emacs.d

--- .emacs.d/packs/pack-kaolin-themes.el
+++
@@ -1,8 +0,0 @@
-(if (not (package-installed-p 'kaolin-themes))
-  (display-warning 'pack-kaolin-themes "not satisfied")
-
-  ;; Configs:
-  (load-theme 'kaolin-dark t)
-  )
-
-(provide 'pack-kaolin-themes)
--- .emacs.d/packs/pack-org.el
+++
@@ -1,8 +0,0 @@
-(if (not (package-installed-p 'org (list 9 5 2)))
-  (display-warning 'pack-org "not satisfied")
-
-  ;; Configs:
-  (setq org-log-done 'time)
-  )
-
-(provide 'pack-org)
