1e5ef4cb4afed3daf2d9f3421f28b17e39f0cdf0
scp <scp@archlinuxscp.xyz>
2022-03-27 00:09:21 +0800

    override function: org-clock-get-clock-string

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -86,11 +86,35 @@
   (setq org-startup-folded 'content)
   (setq org-cycle-emulate-tab nil)
   (setq org-agenda-start-with-log-mode t)
+  (setq org-agenda-start-with-clockreport-mode t)
   (setq org-agenda-start-day "-7d")
   (setq org-agenda-span 15)
   (setq org-agenda-show-current-time-in-grid nil)
   (setq org-agenda-log-mode-items '(closed clock))
   (setq org-log-into-drawer t)
-  (setq org-log-done 'time))
+  (setq org-log-done 'time)
+
+  (defun org-clock-get-clock-string ()
+    "Form a clock-string, that will be shown in the mode line.
+If an effort estimate was defined for the current item, use
+01:30/01:50 format (clocked/estimated).
+If not, show simply the clocked time like 01:50."
+    (let ((clocked-time
+           (floor (org-time-convert-to-integer (time-since org-clock-start-time)) 60)))
+      (if org-clock-effort
+          (let* ((effort-in-minutes (org-duration-to-minutes org-clock-effort))
+                 (work-done-str
+                  (propertize (org-duration-from-minutes clocked-time)
+                              'face
+                              (if (and org-clock-task-overrun
+                                       (not org-clock-task-overrun-text))
+                                  'org-mode-line-clock-overrun
+                                'org-mode-line-clock)))
+                 (effort-str (org-duration-from-minutes effort-in-minutes)))
+            (format (propertize " [%s/%s] (%s)" 'face 'org-mode-line-clock)
+                    work-done-str effort-str org-clock-heading))
+        (format (propertize " [%s] (%s)" 'face 'org-mode-line-clock)
+                (org-duration-from-minutes clocked-time)
+                org-clock-heading)))))
 
 (map! :g "C-s" #'save-buffer)
